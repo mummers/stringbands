46b4fa3840f6719894f953560ebe41fb8a01a918
TJ Ferry <tjtech1014@gmail.com>
2016-06-28 16:07:56 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // Define Google spreadsheet URL
 var mySpreadsheet = 'https://docs.google.com/spreadsheets/d/1xqGTbkgosPqSRCkZ6xKj1c01sRRZkg0qeNeN2hrkFSI/pubhtml?gid=1847002595';
+var lastStandWinners = 'https://docs.google.com/spreadsheets/d/1xqGTbkgosPqSRCkZ6xKj1c01sRRZkg0qeNeN2hrkFSI/pubhtml?gid=639470266'
 
 // Compile the Handlebars template for HR leaders.
 var bandsTemplate = Handlebars.compile($('#bands-template').html());
@@ -29,25 +30,33 @@ if (params == 0) {
     loadResults(createSQL(searchTerm));
 } else if (params['p']){ // Search via button
     searchTerm = params['p'];
-    if (params['p'] == 'firstBands') {
+    if (params['p'] == 'firstBands') { // First Prize Bands
         $('#searchTerm').append("<h2>First Prize Bands</h2>");
         sqlString = "select A,B,C,D,E,F,M,L,U where B = 1 order by A desc";
-    } else if (params['p'] == 'firstCaptains') {
+        loadResults(sqlString);
+    } else if (params['p'] == 'firstCaptains') { // First Prize Captains
         $('#searchTerm').append("<h2>First Prize Captains</h2>");
         sqlString = "select A,B,C,D,E,F,M,L,U where F = 1 order by A desc";
-    } else if (params['p'] == 'randomYear') {
+        loadResults(sqlString);
+    } else if (params['p'] == 'randomYear') { // Random Year
         var year = chance.year({ min: 1902, max: 2016 });
         $('#searchTerm').append("<h2>Search results for &ldquo;" + year + "&rdquo;</h2>");
         sqlString = "select A,B,C,D,E,F,M,L,U where A = " + year + " order by A desc";
-    } else if (params['p'] == 'randomBand') {
+        loadResults(sqlString);
+    } else if (params['p'] == 'randomBand') { // Random Band
         var band = chance.pickset(['Avalon', 'Aqua', 'Broomall', 'Burke', 'Duffy', 'Durning', 'Ferko', 'Fralinger', 'Greater Bucks', 'Greater Kensington', 'Greater Overbrook', 'Harrowgate', 'Hegeman', 'Irish American', 'Italian American', 'Pennsport', 'Polish American', 'Quaker City', 'South Philadelphia', 'Trilby', 'Two Street', 'Ukrainian American', 'Uptown', 'Woodland']);
         $('#searchTerm').append("<h2>Search results for &ldquo;" + band + "&rdquo;</h2>");
         sqlString = "select A,B,C,D,E,F,M,L,U where (lower(C) like lower('%" + band + "%')) order by A desc";
-    } else if (params['p'] == 'leadoffBands') {
+        loadResults(sqlString);
+    } else if (params['p'] == 'leadoffBands') { // Leadoff Bands
         $('#searchTerm').append("<h2>Leadoff Bands</h2>");
         sqlString = "select A,B,C,D,E,F,M,L,U where M = 1 order by A desc";
+        loadResults(sqlString);
+    } else if (params['p'] == 'lastStand') { // Custer's Last Stand Winners
+        $('#searchTerm').append("<h2>Custer's Last Stand Winners</h2>");
+        sqlString2 = "select A,B,C,D,E,F,M,L,U where M = 1 order by A desc";
+        loadResults2(sqlString2);
     }
-    loadResults(sqlString);
 }
 
 // define search string function
@@ -59,6 +68,25 @@ function createSQL(term) {
 function loadResults(sql){
   $('#bands').sheetrock({
     url: mySpreadsheet,
+    sql: sql,
+    rowTemplate: bandsTemplate,
+    callback: function (error, options, response){
+      if(!error){
+        $('#bands').tablesorter();
+        if ($('#bands tr').length == 1) {
+          $('#bands').append("<h3>No results.</h3>")
+        }
+      } else {
+        $('#bands').append('<h3>Error.</h3>');
+      }
+    }
+  });
+}
+
+// define function to load results
+function loadResults2(sql){
+  $('#bands').sheetrock({
+    url: lastStandWinners,
     sql: sql,
     rowTemplate: bandsTemplate,
     callback: function (error, options, response){
