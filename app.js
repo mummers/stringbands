3126e7b09eb77279a85db3d350997ec308fe0930
TJ Ferry <tjtech1014@gmail.com>
2016-06-28 16:25:39 -0400

    Update app.js

--- app.js
+++ app.js
@@ -28,36 +28,32 @@ if (params == 0) {
     searchTerm = params['q'].split('+').join([separator = ' ']).trim();
     $('#searchTerm').append("<h2>Search results for &ldquo;" + searchTerm + "&rdquo;</h2>");
     loadResults(createSQL(searchTerm), mySpreadsheet);
+} else if (params['p'] == 'lastStand') { // Custer's Last Stand Winners
+    $('#searchTerm').append("<h2>Custer's Last Stand Winners</h2>");
+    sqlString = "select A,B,C,D,E,F,M,L,U order by A desc";
+    loadResults(sqlString2, lastStandWinners);
+    $('#bands').append('<h5>The punniest theme title given by Jake Hart.</h5>');
 } else if (params['p']){ // Search via button
     searchTerm = params['p'];
     if (params['p'] == 'firstBands') { // First Prize Bands
         $('#searchTerm').append("<h2>First Prize Bands</h2>");
         sqlString = "select A,B,C,D,E,F,M,L,U where B = 1 order by A desc";
-        loadResults(sqlString, mySpreadsheet);
     } else if (params['p'] == 'firstCaptains') { // First Prize Captains
         $('#searchTerm').append("<h2>First Prize Captains</h2>");
         sqlString = "select A,B,C,D,E,F,M,L,U where F = 1 order by A desc";
-        loadResults(sqlString, mySpreadsheet);
     } else if (params['p'] == 'randomYear') { // Random Year
         var year = chance.year({ min: 1902, max: 2016 });
         $('#searchTerm').append("<h2>Search results for &ldquo;" + year + "&rdquo;</h2>");
         sqlString = "select A,B,C,D,E,F,M,L,U where A = " + year + " order by A desc";
-        loadResults(sqlString, mySpreadsheet);
     } else if (params['p'] == 'randomBand') { // Random Band
         var band = chance.pickset(['Avalon', 'Aqua', 'Broomall', 'Burke', 'Duffy', 'Durning', 'Ferko', 'Fralinger', 'Greater Bucks', 'Greater Kensington', 'Greater Overbrook', 'Harrowgate', 'Hegeman', 'Irish American', 'Italian American', 'Pennsport', 'Polish American', 'Quaker City', 'South Philadelphia', 'Trilby', 'Two Street', 'Ukrainian American', 'Uptown', 'Woodland']);
         $('#searchTerm').append("<h2>Search results for &ldquo;" + band + "&rdquo;</h2>");
         sqlString = "select A,B,C,D,E,F,M,L,U where (lower(C) like lower('%" + band + "%')) order by A desc";
-        loadResults(sqlString, mySpreadsheet);
     } else if (params['p'] == 'leadoffBands') { // Leadoff Bands
         $('#searchTerm').append("<h2>Leadoff Bands</h2>");
         sqlString = "select A,B,C,D,E,F,M,L,U where M = 1 order by A desc";
-        loadResults(sqlString, mySpreadsheet);
-    } else if (params['p'] == 'lastStand') { // Custer's Last Stand Winners
-        $('#searchTerm').append("<h2>Custer's Last Stand Winners</h2>");
-        sqlString = "select A,B,C,D,E,F,M,L,U order by A desc";
-        loadResults(sqlString2, lastStandWinners);
-        $('#bands').append('<h5>The punniest theme title given by Jake Hart.</h5>');
     }
+  loadResults(sqlString, mySpreadsheet);
 }
 
 // define search string function
