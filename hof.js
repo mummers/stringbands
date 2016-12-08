7e7f0907b43a22c8f5cbfc593c6b491e53efd6b8
TJ Ferry <tjtech1014@gmail.com>
2016-12-08 12:37:49 -0500

    Update hof.js

--- hof.js
+++ hof.js
@@ -18,6 +18,7 @@ if(q != undefined){
 
 // Check for parameters
 if (params == 0) {
+    $('#searchTerm').append("<h2>Hall of Fame Inductees</h2>");
     sqlString = "select A,B,C order by A desc";
     loadResults(sqlString, mySpreadsheet);
 }
