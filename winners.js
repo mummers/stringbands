ef6876ddf617fd440c18ea086ae64587191053c4
TJ Ferry <tjtech1014@gmail.com>
2016-12-08 22:55:55 -0500

    Update winners.js

--- winners.js
+++ winners.js
@@ -19,7 +19,7 @@ if(q != undefined){
 // Check for parameters
 if (params == 0) {
     $('#searchTerm').append("<h2>Band Win Tally</h2>");
-    sqlString = "select A,B order by B desc";
+    sqlString = "select A,B,C,D order by B desc";
     loadResults(sqlString, mySpreadsheet);
 }
 
