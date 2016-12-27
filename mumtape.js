e9f54e687c93f9543dca4b67173ea5f4ea6dc25f
TJ Ferry <tjtech1014@gmail.com>
2016-12-26 22:45:00 -0500

    Use new array function

--- mumtape.js
+++ mumtape.js
@@ -4,27 +4,37 @@ $('#searchTerm').append("<h2>Random Mum Tape</h2>");
 sqlString = "select A,V where V contains 'https' order by A desc";
 loadResults(sqlString);
 
-function loadResults(sql){
+function loadResults(sql) {
   $('#themes').sheetrock({
     url: mySpreadsheet,
-    query: sql,
+    query: sql
   });
 }
 
-var delay=700;
+var delay = 700;
 setTimeout(function() {
-	var video = ($('#themes td:nth-child(2)').map(function(){
-	   return $(this).text();
-	}).get());
-  randomVid=chance.pickone(video);
-  ID = randomVid.replace("https://www.youtube.com/watch?v=", "");
+  var myTableArray = [];
+  $("table#themes tr").each(function() {
+    var arrayOfThisRow = [];
+    var tableData = $(this).find('td');
+    if (tableData.length > 0) {
+      tableData.each(function() {
+        arrayOfThisRow.push($(this).text());
+      });
+      myTableArray.push(arrayOfThisRow);
+    }
+  });
+  randomVid = chance.pickone(myTableArray);
+  ID = randomVid[1].replace("https://www.youtube.com/watch?v=", "");
   message = '<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/' + ID + '"></iframe>';
+  year = randomVid[0];
   document.getElementById('themes').innerHTML = message;
   document.getElementById("themes").style.visibility = "visible";
+  document.getElementById('year').innerHTML = 'Check out the results for <a href="index.html?q="' + year + '>' + year + '</a>.';
   apiKey = "AIzaSyBkW3JBO9VU6eI-2Ee9G4QfKV-gdcmDK70"
-  $.getJSON( "https://www.googleapis.com/youtube/v3/videos?part=id%2Csnippet&id=" + ID + "&key=" + apiKey, function( data ) {
+  $.getJSON("https://www.googleapis.com/youtube/v3/videos?part=id%2Csnippet&id=" + ID + "&key=" + apiKey, function(data) {
     videoTitle = data.items[0].snippet.localized.title;
-    document.getElementById('searchTerm').innerHTML = "<h2>Random Mum Tape: "+ videoTitle + "</h2>";
+    document.getElementById('searchTerm').innerHTML = "<h2>Random Mum Tape: " + videoTitle + "</h2>";
   });
 
 }, delay);
