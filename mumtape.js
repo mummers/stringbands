09a58ed4ffb74209cdab8b88b36e6e42a2b703b8
TJ Ferry <tjtech1014@gmail.com>
2016-12-30 18:34:33 -0500

    Remove API Key need on video title

--- mumtape.js
+++ mumtape.js
@@ -1,7 +1,7 @@
 var mySpreadsheet = 'https://docs.google.com/spreadsheets/d/1xqGTbkgosPqSRCkZ6xKj1c01sRRZkg0qeNeN2hrkFSI/pubhtml?gid=1847002595';
 $('#searchTerm').append("<h2>Random Mum Tape</h2>");
 
-sqlString = "select A,V where V contains 'https' order by A desc";
+sqlString = "select A,B,C,V where V contains 'https' order by A desc";
 loadResults(sqlString);
 
 function loadResults(sql) {
@@ -25,16 +25,12 @@ setTimeout(function() {
     }
   });
   randomVid = chance.pickone(myTableArray);
-  ID = randomVid[1].replace("https://www.youtube.com/watch?v=", "");
+  ID = randomVid[3].replace("https://www.youtube.com/watch?v=", "");
   message = '<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/' + ID + '"></iframe>';
   year = randomVid[0];
+  var band = randomVid[2];
   document.getElementById('themes').innerHTML = message;
   document.getElementById("themes").style.visibility = "visible";
   document.getElementById('year').innerHTML = 'Check out the results for <a href="index.html?q=' + year + '" target="_blank">' + year + '</a>.';
-  apiKey = "AIzaSyBkW3JBO9VU6eI-2Ee9G4QfKV-gdcmDK70"
-  $.getJSON("https://www.googleapis.com/youtube/v3/videos?part=id%2Csnippet&id=" + ID + "&key=" + apiKey, function(data) {
-    videoTitle = data.items[0].snippet.localized.title;
-    document.getElementById('searchTerm').innerHTML = "<h2>Random Mum Tape: " + videoTitle + "</h2>";
-  });
-
+  document.getElementById('searchTerm').innerHTML = "<h2>Random Mum Tape: " + year + " " + band + " String Band</h2>";
 }, delay);
