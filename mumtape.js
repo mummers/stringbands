c9e1a6c1f12856e6ba61783276d01ad21a2450fb
TJ Ferry <tjtech1014@gmail.com>
2016-12-30 19:19:59 -0500

    Update mumtape.js

--- mumtape.js
+++ mumtape.js
@@ -2,12 +2,13 @@ var mySpreadsheet = 'https://docs.google.com/spreadsheets/d/1xqGTbkgosPqSRCkZ6xK
 $('#searchTerm').append("<h2>Random Mum Tape</h2>");
 
 sqlString = "select A,B,C,V where V contains 'https' order by A desc";
-loadResults(sqlString);
+loadResults(sqlString, '#themes');
 
-function loadResults(sql) {
-  $('#themes').sheetrock({
+function loadResults(sql, table) {
+  $(table).sheetrock({
     url: mySpreadsheet,
-    query: sql
+    query: sql,
+    labels: ['Year', 'Band Prize', 'Band', 'Theme Title', 'Captain', 'Captain Prize']
   });
 }
 
@@ -28,9 +29,21 @@ setTimeout(function() {
   ID = randomVid[3].replace("https://www.youtube.com/watch?v=", "");
   message = '<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/' + ID + '"></iframe>';
   year = randomVid[0];
-  var band = randomVid[2];
+  band = randomVid[2];
   document.getElementById('themes').innerHTML = message;
   document.getElementById("themes").style.visibility = "visible";
-  document.getElementById('year').innerHTML = 'Check out the results for <a href="index.html?q=' + year + '" target="_blank">' + year + '</a>.';
+  document.getElementById('year-tag').innerHTML = '<a href="index.html?q=' + year + '">' + 'Check out the full results for ' + year + ' here</a>.';
   document.getElementById('searchTerm').innerHTML = "<h2>Random Mum Tape: " + year + " " + band + " String Band</h2>";
+  sqlString = "select A,B,C,D,E,F where A = " + year + " order by A desc";
+  loadResults(sqlString, '#results');
 }, delay);
+
+setTimeout(function() {
+  var links = document.getElementsByTagName("tr");
+
+  for (var i = 0; i < links.length; i++) {
+    if (links[i].innerHTML.includes(band)) {
+      links[i].className = "info";
+    }
+  }
+}, 1200);
