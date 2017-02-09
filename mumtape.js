9d7b77c188f2dda407159b978883455b73719ade
TJ Ferry <tjtech1014@gmail.com>
2017-02-08 21:35:57 -0500

    Update mumtape.js

--- mumtape.js
+++ mumtape.js
@@ -101,7 +101,7 @@ setTimeout(function() {
 		document.getElementById('searchTerm').innerHTML = "<h2>Whoops! Looks like there was an error. Try again.</h2>";
 		document.getElementById("themes").style.visibility = "visible";
 		if (year > 2017){
-			year = year + 1
+			year = parseInt(year) + 1
 			document.getElementById('themes').innerHTML = "<h2>" + year +  " didn't happen yet!</h2>";
 		}
 		else {
